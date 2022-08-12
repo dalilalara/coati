28f4e4e874a1fd004890b508b06b50d1912dbee9
dalilalara <dalilanlara@gmail.com>
2022-08-12 14:48:31 -0700

    added function to calculate animal population of mexico

---
+++ R/animals_sum.R
@@ -0,0 +1,14 @@
+
+#' Calculation animal population of Quintana Roo, Mexico
+#'
+#' @param coati animal population
+#' @param lizards animal population
+#' @param fish animal population
+#'
+#' @return
+#' @export
+#'
+#' @examples
+animals_sum <- function(coati, lizards, fish) {
+  (coati + lizards + fish)
+}
